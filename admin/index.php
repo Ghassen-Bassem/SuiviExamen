ed90c71d547c6ff18865df7e65a3e10ef0c43f0a
Ghassen-Bassem <bha.ghassen@gmail.com>
2022-05-18 09:52:30 +0100

    index version 2

---
+++ admin/index.php
@@ -0,0 +1,18 @@
+<?php 
+session_start(); 
+include('includes/header.php'); 
+include('includes/navbar2.php'); 
+?>
+<!-- Page Heading -->
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+ <h1 class="h3 mb-0 text-gray-800">Bienvenue</h1>
+ <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadowsm"><i
+ class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+</div>
+<!-- Ajout -->
+</div>
+</div>
+<?php 
+include('includes/footer.php'); 
+include('includes/scripts.php'); 
+?>
